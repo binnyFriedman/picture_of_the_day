cb756816ce6fc1520a812ede9a16ad9e431d1933
Binny Friedman <binny@nekuda.co.il>
2022-02-21 21:22:54 +0100

    Removed the scheduler.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,22 @@
 use std::io::{Write};
 use std::fs::File;
 use std::collections::HashMap;
-use std::{fs, io};
+use std::{fs};
 use std::path::Path;
 use chrono::{Datelike, Utc};
-use clokwerk::{AsyncScheduler, Job, TimeUnits};
 use walkdir::{DirEntry, WalkDir};
 
 #[tokio::main]
 async fn main() {
-    picture_of_the_day_to_data_folder().await;
-    schedule_downloads().await;
-}
-
-async fn picture_of_the_day_to_data_folder(){
     picture_of_the_day("data/archive/","data/pictures").await;
+    println!("Downloaded picture of the day {}", Utc::today().format("%Y-%m-%d"));
 }
 
- async fn schedule_downloads() {
-    //use clockwork to run the file download every day at 9:00 am
-    let mut scheduler = AsyncScheduler::new();
-    scheduler.every(1.day()).at("09:00").run(picture_of_the_day_to_data_folder);
-
-
-     //keep the main thread alive by looping forever
-     //put the thread to sleep for 10 minutes
-     loop {
-         scheduler.run_pending().await;
-         std::thread::sleep(std::time::Duration::from_secs(600));
-     }
-}
 
 async fn picture_of_the_day(archive_dir: &str, download_dir: &str) {
-    let url =  get_picture_of_the_day_url() .await.expect("Could not get picture of the day url");
+    let url =  get_picture_of_the_day_url().await.expect("Could not get picture of the day url");
     move_older_to_archive(download_dir,archive_dir).expect("Could not move older files to archive");
-    download_file(url,download_dir).await.expect("Could not download file");
+    download_file(&url,download_dir).await.expect("Could not download file");
 }
 
 fn get_date()-> String {
@@ -48,8 +30,16 @@ fn get_date()-> String {
 
 async fn get_picture_of_the_day_url()->Result<String,String> {
     let endpoint = "https://bing.biturl.top";
-    let resp = reqwest::get(endpoint).await.unwrap();
-    let body = resp.json::<HashMap<String, String>>().await.unwrap();
+    let resp = reqwest::get(endpoint).await;
+    if resp.is_err() {
+        return Err(format!("Could not get response from {}", endpoint));
+    }
+    let resp = resp.unwrap();
+    let body = resp.json::<HashMap<String, String>>().await;
+    if body.is_err() {
+        return Err(format!("Could not get json from {}", endpoint));
+    }
+    let body = body.unwrap();
 
    let url =  body.get("url");
    match url {
@@ -58,16 +48,53 @@ async fn get_picture_of_the_day_url()->Result<String,String> {
    }
 }
 
-async fn download_file(target: String,destination:&str)->Result<File,reqwest::Error> {
-    let response = reqwest::get(target).await?;
+fn get_file_extension(response: &reqwest::Response)-> Result<String, String> {
+    let header = response.headers().get("content-type");
+    if header.is_none() {
+        return Err("No content-type header found".to_string());
+    }
+    let header = header.unwrap();
+
+    let ext = header.to_str();
+    let ext = match ext {
+        Ok(ext) => ext,
+        Err(_) => return Err("Could not get file extension".to_string())
+    };
+    let ext = ext.split("/").collect::<Vec<&str>>();
+    let ext = ext.last();
+    if ext.is_none() {
+        return Err("Could not get file extension".to_string());
+    }
+    let ext = ext.unwrap();
+ 
+    Ok(ext.to_string())
+}
+
+async fn download_file(target: &str,destination:&str)->Result<File,String> {
+    let response = reqwest::get(target).await;
+    if response.is_err() {
+        return Err(format!("Could not get response from {}", target));
+    }
+    let response = response.unwrap();
     let file_name = get_date();
-    let ext = response.headers().get("content-type").unwrap().to_str().unwrap();
-    let ext = ext.split("/").last().unwrap();
+    let ext = match get_file_extension(&response){
+        Ok(ext) => ext,
+        Err(_) => "txt".to_string()
+    };
+    
+
     let mut file = File::create(format!("{}/{}.{}",destination,file_name,ext))
         .expect(format!("Could not create file in destination {}",destination).as_str());
-    let mut bytes = response.bytes().await?;
-    file.write_all(&mut bytes).expect("Could not write file the downloaded file");
-    Ok(file)
+    let bytes = response.bytes().await;
+    if bytes.is_err() {
+        return Err(format!("Could not get bytes from {}", target));
+    }
+    let mut bytes = bytes.unwrap();
+    let result = file.write_all(&mut bytes);
+    match result {
+        Ok(_) => Ok(file),
+        Err(e) => Err(format!("Could not write to file {}", e))
+    }
 }
 
 fn is_not_hidden(entry: &walkdir::DirEntry) -> bool {
@@ -78,10 +105,14 @@ fn is_not_hidden(entry: &walkdir::DirEntry) -> bool {
          .unwrap_or(false)
 }
 
-fn create_path_if_not_exists(path:&str){
+fn create_path_if_not_exists(path:&str)->Result<(),String> {
     if !Path::new(path).exists() {
-        fs::create_dir_all(path).unwrap();
+       if fs::create_dir_all(path).is_err() {
+             return Err("Could not create path".to_string());
+       }
     }
+
+    Ok(())
 }
 
 fn loop_over_dir<F:FnMut(walkdir::DirEntry)>(dir:&str, callback:F){
@@ -92,10 +123,10 @@ fn loop_over_dir<F:FnMut(walkdir::DirEntry)>(dir:&str, callback:F){
 
 }
 
-fn move_older_to_archive(from:&str, to:&str)->Result<(),io::Error>{
+fn move_older_to_archive(from:&str, to:&str)->Result<(),String>{
 
-    create_path_if_not_exists(from);
-    create_path_if_not_exists(to);
+    create_path_if_not_exists(from)?;
+    create_path_if_not_exists(to)?;
 
    let  move_to_archive = |entry:DirEntry|{
         if !entry.metadata().unwrap().is_dir() {
@@ -109,19 +140,17 @@ fn move_older_to_archive(from:&str, to:&str)->Result<(),io::Error>{
 }
 
 pub mod test {
-    use std::fs::File;
+    use super::*;
     use std::path::Path;
-    use crate::{create_path_if_not_exists, move_older_to_archive};
-    use walkdir::{WalkDir};
-
+    use std::fs::{ File};
     #[test]
     fn _older_to_archive(){
        //create 2 files in a temp dir
        let temp_dir = "data/temp/";
        let archive_dir = "data/temp_archive/";
 
-        create_path_if_not_exists(temp_dir);
-        create_path_if_not_exists(archive_dir);
+        create_path_if_not_exists(temp_dir).unwrap();
+        create_path_if_not_exists(archive_dir).unwrap();
 
         let file1_temp = format!("{}/file1.txt",temp_dir);
         let file2_temp = format!("{}/file2.txt",temp_dir);
@@ -154,7 +183,7 @@ pub mod test {
     #[test]
     fn create_if_not_exists(){
         let path = "temp/archive/";
-        crate::create_path_if_not_exists(path);
+        create_path_if_not_exists(path).unwrap();
         assert!(Path::new(path).exists());
         //clean up
         std::fs::remove_dir_all(path).unwrap();
@@ -164,7 +193,7 @@ pub mod test {
     #[test]
     fn _is_not_hidden(){
         let entry = WalkDir::new(".").into_iter().next().unwrap().unwrap();
-        assert!(crate::is_not_hidden(&entry));
+        assert!(is_not_hidden(&entry));
     }
 
     #[test]
